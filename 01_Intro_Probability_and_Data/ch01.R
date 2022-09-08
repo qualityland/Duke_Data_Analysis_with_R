458ef657f194f112c391b7e75ab1730c68573a98
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-09-08 17:41:29 +0200

    chapter 1 started

--- 01_Intro_Probability_and_Data/ch01.R
+++ 01_Intro_Probability_and_Data/ch01.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-### chapter 1.1
+### Chapter 1.1 Case study: using stents to prevent strokes
 
 # show all data sets in package
 data(package = "openintro")
@@ -32,7 +32,6 @@ df
 # 1.1
 data(migraine, package = "openintro")
 table(migraine)
-
 # (a) What percent of patients in the treatment group were pain free 24 hours
 #     after receiving acupuncture?
 10 / 43
@@ -48,21 +47,67 @@ table(migraine)
 #     treatment for migraines for all people
 # Answer: Possible, but not very probable that result is due to chance.
 
+# 1.2
+data(sinusitis, package = "openintro")
+table(sinusitis)
 
-### chapter 1.2
+# (a) What percent of patients in the treatment group experienced improvement in
+#     symptoms?
+66 / 85
 
-data(email50, package = "openintro")
-head(email50)
+# (b) What percent experienced improvement in symptoms in the control group?
+65 / 81
+
+# (c) In which group did a higher percentage of patients experience 
+#     improvement in symptoms?
+# Answer: control group
+
+# (d) Your findings so far might suggest a real difference in effectiveness of
+#     antibiotic and placebo treatments for improving symptoms of sinusitis.
+#     However, this is not the only possible conclusion that can be drawn
+#     based on your findings so far. What is one other possible explanation for
+#     the observed difference between the percentages of patients in the
+#     antibiotic and placebo treatment groups that experience improvement in
+#     symptoms of sinusitis?
+# Answer: Very probable that result is due to chance.
 
 
-### exercise 1.2
+
+
+### Chapter 1.2 Data Basics
+
+data(loan50, package = "openintro")
+head(loan50)
+?openintro::loan50
+
 county <- read_csv("data/county.csv")
-county
-names(county)
-# Answer: 3142 observations, 15 variables
+head(county)
+str(county)
+
+# 1.2.2 Types of Variables
+
+# Example 1.5
+# Data were collected about students in a statistics course.
+# Three variables were recorded for each student: 
+#   a) number of siblings,
+#   b) student height
+#   c) whether the student had previously taken a statistics course.
+# Classify each of the variables as continuous numerical, discrete numerical,
+# or categorical.
+# Answer:
+#   a) discrete numerical
+#   b) coontiuous numerical
+#   c) categorical
+
+# Guided Practice 1.6
+# group (treatment or control): categorical
+# num_migraines: numerical discrete
 
 
-### chapter 1.2.2
 
-county %>% 
-  select(per_capita_income, pop2010, state, smoking_ban)
+# 1.2.3 Relationship between Variables
+
+
+
+
+
